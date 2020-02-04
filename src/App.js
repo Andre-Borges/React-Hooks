b2aa2cb90f4a1e78b24871363d98e7fb361edadc
André Borges <hoilut@hotmail.com>
2020-02-03 23:54:48 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+// useState -> criar estados na função sem escrever elas em formato de classe.
+// useEffect -> Sobrepões metodos do ciclo de vida (componentDidMount(), componentDidUpdate(), componentWillUnmount())
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  // [estado, função para atualizar estado]
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  // Executa uma unica vez -> componentDidMount()
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // Executa toda vez que o valor de tech for alterado -> componentDidUpdate()
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
